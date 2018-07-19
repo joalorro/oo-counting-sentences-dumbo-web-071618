e5a4f61b9394a70066c244ec2bbdf644101022e4
joalorro <joalorro@gmail.com>
2018-07-18 20:24:07 -0400

    finished

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
   def count_sentences
-
+  	self.split(' ').find_all {|word| word.end_with?(".") || word.end_with?("!") || word.end_with?("?") }.length
   end
+
 end
